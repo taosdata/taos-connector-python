3794de76011acd464782220a7615033635b4ce23
dingbo <dingbo8128@163.com>
2022-04-16 15:55:43 +0800

    docs: update user manual

---
+++ taosres/connection.py
@@ -0,0 +1,15 @@
+class TaosRestConnection:
+    def __init__(self):
+        pass
+
+    def close(self):
+        pass
+
+    def commit(self):
+        pass
+
+    def rollback(self):
+        pass
+
+    def cursor(self):
+        pass
---
+++ taosres/cursor.py
@@ -0,0 +1,45 @@
+# For API reference, please refer to: https://peps.python.org/pep-0249/#cursor-objects
+
+from errors import *
+
+
+class TaosRestCursor:
+    def __init__(self):
+        self.description = []
+        self.rowcount = -1
+        self.arraysize = 1
+
+    def callproc(self, procname, parameters=None):
+        raise NotSupportedError()
+
+    def close(self):
+        pass
+
+    def execute(self, operation, parameters=None):
+        pass
+
+    def executemany(self, operation, parameters=None):
+        raise NotSupportedError()
+
+    def fetchone(self):
+        pass
+
+    def fetchmany(self):
+        pass
+
+    def fetchall(self):
+        pass
+
+    def nextset(self):
+        raise NotSupportedError()
+
+    def setinputsizes(self):
+        raise NotSupportedError()
+
+    def setoutputsize(self, size, column=None):
+        raise NotSupportedError()
+
+    def setoutputsize(self, size, column=None):
+        raise NotSupportedError()
+
+
