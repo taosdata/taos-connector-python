d0cf2d340f7f723ded81a1ec95b46ae7593ae692
dingbo <dingbo8128@163.com>
2022-04-19 13:09:04 +0800

    feat(restful): add some test cases

--- taosres/connection.py
+++ taosres/connection.py
@@ -1,9 +1,9 @@
 class TaosRestConnection:
     def __init__(self, **kwargs):
-        """
-
-        """
-        pass
+        self._host = kwargs["host"] if "host" in kwargs else "localhost"
+        self._port = kwargs["port"] if "port" in kwargs else 6041
+        self._user = kwargs["user"] if "user" in kwargs else "root"
+        self._password = kwargs["password"] if "password" in kwargs else "taosdata"
 
     def close(self):
         pass
