a4e42a3f148559d2cc2a54644624b7e77ac42372
Huo Linhe <linhehuo@gmail.com>
2022-05-17 21:05:07 +0800

    test: test stmt bind passed in 3.0

--- tests/test_stmt.py
+++ tests/test_stmt.py
@@ -25,7 +25,7 @@ def test_stmt_insert(conn):
              bi bigint, tu tinyint unsigned, su smallint unsigned, iu int unsigned, bu bigint unsigned, \
              ff float, dd double, bb binary(100), nn nchar(100), tt timestamp)",
         )
-        conn.load_table_info("log")
+        # conn.load_table_info("log")
         
 
         stmt = conn.statement("insert into log values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
@@ -50,14 +50,9 @@ def test_stmt_insert(conn):
         stmt.bind_param(params)
         stmt.execute()
 
-        result = stmt.use_result()
-        assert result.affected_rows == 1
-        result.close()
-        stmt.close()
+        assert stmt.affected_rows == 1
 
-        stmt = conn.statement("select * from log")
-        stmt.execute()
-        result = stmt.use_result()
+        result = conn.query("select * from log")
         row  = result.next()
         print(row)
         assert row[2] is None
@@ -91,11 +86,11 @@ def test_stmt_insert_multi(conn):
              bi bigint, tu tinyint unsigned, su smallint unsigned, iu int unsigned, bu bigint unsigned, \
              ff float, dd double, bb binary(100), nn nchar(100), tt timestamp)",
         )
-        conn.load_table_info("log")
+        # conn.load_table_info("log")
 
         start = datetime.now()
         stmt = conn.statement("insert into log values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
-        
+
         params = new_multi_binds(16)
         params[0].timestamp((1626861392589, 1626861392590, 1626861392591))
         params[1].bool((True, None, False))
@@ -114,29 +109,11 @@ def test_stmt_insert_multi(conn):
         params[14].nchar(["涛思数据", None, "a long string with 中文字符"])
         params[15].timestamp([None, None, 1626861392591])
         stmt.bind_param_batch(params)
-        
         stmt.execute()
         end = datetime.now()
         print("elapsed time: ", end - start)
-        result = stmt.use_result()
-        assert result.affected_rows == 3
-        result.close()
+        assert stmt.affected_rows == 3
         stmt.close()
-
-        stmt = conn.statement("select * from log")
-        stmt.execute()
-        result = stmt.use_result()
-        for row in result:
-            print(row)
-        result.close()
-
-        stmt.close()
-
-        # start = datetime.now()
-        # conn.query("insert into log values(1626861392660, true, NULL, 0, 3,3,3,3,3,3,3,3.0,3.0, 'abc','涛思数据',NULL)(1626861392661, true, NULL, 0, 3,3,3,3,3,3,3,3.0,3.0, 'abc','涛思数据',NULL)(1626861392662, true, NULL, 0, 3,3,3,3,3,3,3,3.0,3.0, 'abc','涛思数据',NULL)")
-
-        # end = datetime.now()
-        # print("elapsed time: ", end - start)
 
         conn.execute("drop database if exists %s" % dbname)
         conn.close()
