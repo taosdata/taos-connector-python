ee38c7dd668460974fc438152473d44c09d0f12f
dingbo8128 <dingbo8128@163.com>
2022-06-20 11:46:52 +0800

    docs: update build-docs.sh

--- ci/build-doc.sh
+++ ci/build-doc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# pip3 install pdoc3
+pip3 install pdoc3
 source .env
-cd ..
-pdoc --html --force --output-dir docs taosrest
-pdoc --html --force --output-dir docs taos
-sshpass -p "$DOC_HOST_PWD"  scp -r ./docs/* root@$DOC_HOST:/data/apidocs/taospy/
+pdoc --html --force --output-dir ../docs taosrest
+pdoc --html --force --output-dir ../docs taos
+sshpass -p "$DOC_HOST_PWD"  scp -r ../docs/* root@$DOC_HOST:/data/apidocs/taospy/
