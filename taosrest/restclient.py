e0705baecbd1b400748322277d974895e16760e2
dingbo <dingbo8128@163.com>
2022-07-01 11:55:46 +0800

    enh: restclient.py

--- taosrest/restclient.py
+++ taosrest/restclient.py
@@ -4,7 +4,15 @@ from urllib.request import urlopen, Request
 
 from iso8601 import parse_date
 
-from .errors import ConnectionError, ExecutionError
+from .errors import ConnectError, ExecutionError, HTTPError
+
+error_msgs = {
+    400: "parameter error",
+    401: "authorization error",
+    404: "api not found",
+    500: "internal error",
+    503: "system resources is not sufficient. It is may be caused by a huge query."
+}
 
 
 class RestClient:
@@ -13,7 +21,12 @@ class RestClient:
      For detailed info about TDengine REST API refer https://docs.tdengine.com/reference/rest-api/
     """
 
-    def __init__(self, url: str, token: str = None, database: str = None, user: str = "root", password: str = "taosdata", timeout: int = None, convert_timestamp=False):
+    def __init__(self, url: str, token: str = None,
+                 database: str = None,
+                 user: str = "root",
+                 password: str = "taosdata",
+                 timeout: int = None,
+                 convert_timestamp=True):
         """
         Create a RestClient object.
 
@@ -25,7 +38,8 @@ class RestClient:
         - user : username used to log in, optional
         - password : password used to log in, optional
         - timeout : the optional timeout parameter specifies a timeout in seconds for blocking operations
-        - convert_timestamp: whether convert timestamp from type str to type datetime
+        - convert_timestamp: whether to convert timestamp from type str to type datetime with tzinfo.
+            The default timezone is UTC. You can use method `datatime.astimezone()` to convert it to your local time.
         """
         # determine schema://host:post
         self._url = url.strip('/')
@@ -43,7 +57,10 @@ class RestClient:
             self._headers = {}
         else:
             self._login_url = f"{self._url}/rest/login/{user}/{password}"
-            self._sql_utc_url = f"{self._url}/rest/sql"
+            if not database:
+                self._sql_utc_url = f"{self._url}/rest/sql"
+            else:
+                self._sql_utc_url = f"{self._url}/rest/sql/{database}"
             self._taosd_token = self.get_taosd_token()
             self._headers = {
                 "Authorization": "Taosd " + self._taosd_token
@@ -55,9 +72,11 @@ class RestClient:
         Get authorization token.
         """
         response = urlopen(self._login_url, timeout=self._timeout)
+        self._check_status(response)
+
         resp = json.load(response)
         if resp["code"] != 0:
-            raise ConnectionError(resp["desc"], resp["code"])
+            raise ConnectError(resp["desc"], resp["code"])
         return resp["desc"]
 
     def sql(self, q: str) -> dict:
@@ -74,8 +93,9 @@ class RestClient:
         data = q.encode("utf8")
         request = Request(self._sql_utc_url, data, self._headers)
         response = urlopen(request, timeout=self._timeout)
+        self._check_status(response)
         resp = json.load(response)
-        if resp["status"] == "error":
+        if resp["code"] != 0:
             raise ExecutionError(resp["desc"], resp["code"])
         if self._convert_timestamp:
             self._convert_time(resp)
@@ -95,4 +115,11 @@ class RestClient:
         if len(ts_cols) > 0:
             for row in data:
                 for i in ts_cols:
-                    row[i] = parse_date(row[i])
+                    if row[i]:
+                        row[i] = parse_date(row[i])
+
+    def _check_status(self, response):
+        status = response.status
+        if status != 200:
+            msg = error_msgs.get(status)
+            raise HTTPError(status, msg)
