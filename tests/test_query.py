839ae94d6dbb5734338dc5a47dadf6429902698a
sunpeng <sunpeng.dev@gmail.com>
2023-02-21 15:36:35 +0800

    enh: delete TaosConsumer and TaosTmq api

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,46 +1,45 @@
+import multiprocessing
+import time
 from datetime import datetime
 
 import taos
 
+from multiprocessing import pool
+
+
+def test_cursor():
+    conn = taos.connect(host='192.168.1.95', port=6030)
+    cursor = conn.cursor()
+    while True:
+        cursor.execute("select count(*) from test_query_py.tb1 limit 1")
+        print(cursor.fetchall())
+        time.sleep(1)
+        conn.execute(
+            "insert into test_query_py.tn1 using test_query_py.tb1 tags('{\"name\":\"value\"}') "
+            "values(now, null) (now + 10s, 1)")
+
 
 def test_query():
     """This test will use fetch_block for rows fetching, significantly faster than rows_iter"""
-    conn = taos.connect()
-    conn.execute("drop database if exists test_query_py")
-    conn.execute("create database if not exists test_query_py")
-    conn.execute("use test_query_py")
-    conn.execute("create table if not exists tb1 (ts timestamp, v int) tags(jt json)")
-    n = conn.execute("insert into tn1 using tb1 tags('{\"name\":\"value\"}') values(now, null) (now + 10s, 1)")
-    n = conn.execute("insert into tn1 using tb1 tags('{\"name\":\"value\"}') values(now, null) (now + 10s, 1)")
-    print("inserted %d rows" % n)
-    result = conn.query("select * from tb1")
-    fields = result.fields
-    for field in fields:
-        print("field: %s" % field)
-
-    # test re-consume fields
-    flag = 0
-    for _ in fields:
-        flag += 1
-    assert flag == 3
-
     start = datetime.now()
-    for row in result:
-        print(row)
-        None
 
-    for row in result.rows_iter():
-        print(row)
-    
-    result = conn.query("select * from tb1 limit 1")
-    results = result.fetch_all_into_dict()
-    print(results)
+    multiprocessing.set_start_method('spawn')
+    p = pool.Pool(10)
+
+    async_result = []
+    for _ in range(10):
+        async_result.append(p.apply_async(func=test_cursor))
+
+    for r in async_result:
+        r.get()
+
+    p.close()
+    p.join()
 
     end = datetime.now()
     elapsed = end - start
     print("elapsed time: ", elapsed)
-    result.close()
-    conn.close()
+
 
 if __name__ == "__main__":
     test_query()
