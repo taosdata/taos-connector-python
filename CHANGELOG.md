71792545f4d5352575837f8e936f29134076f0d6
Shuduo Sang <sangshuduo@gmail.com>
2023-02-27 12:14:16 +0800

    release: v2.7.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## v2.7.6 - 2023-02-27
+
+### Bug Fixes:
+
+- add pandas-read-sql-tdengine-cloud.py (#134)
+- fix for sqlalchemy warning (#135)
+
 ## v2.7.5 - 2023-02-23
 
 ### Bug Fixes:
--- examples/pandas-read-sql.py
+++ examples/pandas-read-sql.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, text
 protocol_and_port = {'taos': 6030, 'taosrest': 6041, 'taosws': 6041}
 
 for (protocol, port) in protocol_and_port.items():
-    engine = create_engine(f"{protocol}://root:taosdata@localhost:{port}")
+    engine = create_engine(f"{protocol}://root:taosdata@localhost")
     conn = engine.connect()
     res = pandas.read_sql(text("show databases"), conn)
     conn.close()
