6eb3dfc6701fb262173c54831f87c49de095e8a2
AdamJi <jrgwyz@163.com>
2023-03-10 17:23:00 +0800

    test: add comment

--- tests/test_connect_args.py
+++ tests/test_connect_args.py
@@ -2,6 +2,12 @@ from taos import *
 
 
 def test_connect_args():
+    """
+    DO NOT DELETE THIS TEST CASE!
+
+    Useless args, prevent mistakenly deleted args in connect init.
+    Because some case in CI of earlier version may use it.
+    """
     host = 'localhost:6030'
     conn = connect(host)
     assert conn is not None
