d91d5ed04ab74d8d47eb586c8d3daa1b8d550c3a
AdamJi <jrgwyz@163.com>
2023-03-22 17:12:10 +0800

    test: add case req_id for ws

--- taos-ws-py/tests/test_req_id.py
+++ taos-ws-py/tests/test_req_id.py
@@ -1,4 +1,5 @@
 import taosws
+import datetime
 
 
 def test_query():
@@ -32,3 +33,26 @@ def test_cursor_execute_many():
     data = [{"name": "tb1", "t1": 1}, {"name": "tb2", "t1": 2}]
     res = cur.execute_many_with_req_id("create table {name} using stb tags({t1})", data, 1)
     print(f'res: {res}')
+
+
+def test_full():
+    ws = taosws.connect("taosws://root:taosdata@localhost:6041")
+    cur = ws.cursor()
+    res = cur.execute_with_req_id('show dnodes', 1)
+    print(f'res: {res}')
+    db = "t_ws"
+    cur.execute("drop database if exists {}", db)
+    cur.execute("create database {}", db)
+    cur.execute("use {name}", name=db)
+    cur.execute("create stable stb (ts timestamp, v1 int) tags(t1 int)")
+
+    data = [{"name": "tb1", "t1": 1}, {"name": "tb2", "t1": 2}]
+    res = cur.execute_many_with_req_id("create table {name} using stb tags({t1})", data, 1)
+    print(f'res: {res}')
+    ts = datetime.datetime.now().astimezone()
+    data = [("tb1", ts, 1), ("tb2", ts, 2)]
+    cur.execute_many("insert into {} values('{}', {})", data)
+    cur.execute_with_req_id('select * from stb', 1)
+    row = cur.fetchone()
+    print(f'row: {row}')
+    assert row is not None
