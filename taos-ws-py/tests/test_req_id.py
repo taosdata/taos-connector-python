51923a1d07c30e60514fa48d591282b04c3dd6b8
AdamJi <jrgwyz@163.com>
2023-03-22 16:48:02 +0800

    test: add case req_id for ws

--- taos-ws-py/tests/test_req_id.py
+++ taos-ws-py/tests/test_req_id.py
@@ -18,3 +18,10 @@ def test_cursor_execute():
     cur = ws.cursor()
     res = cur.execute_with_req_id('show dnodes', 1)
     print(f'res: {res}')
+
+
+def test_cursor_execute_many():
+    ws = taosws.connect("taosws://root:taosdata@localhost:6041")
+    cur = ws.cursor()
+    res = cur.execute_many_with_req_id('show dnodes', req_id=1)
+    print(f'res: {res}')
