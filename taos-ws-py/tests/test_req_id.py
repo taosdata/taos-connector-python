512c1be1cf2a5fe075392f5c509c7e83d371261a
AdamJi <jrgwyz@163.com>
2023-03-22 16:50:58 +0800

    test: add case req_id for ws

--- taos-ws-py/tests/test_req_id.py
+++ taos-ws-py/tests/test_req_id.py
@@ -30,5 +30,5 @@ def test_cursor_execute_many():
     cur.execute("create stable stb (ts timestamp, v1 int) tags(t1 int)")
 
     data = [{"name": "tb1", "t1": 1}, {"name": "tb2", "t1": 2}]
-    res = cur.execute_many_with_req_id("create table {name} using stb tags({t1})", data)
+    res = cur.execute_many_with_req_id("create table {name} using stb tags({t1})", data, 1)
     print(f'res: {res}')
