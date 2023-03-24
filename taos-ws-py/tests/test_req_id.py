754d71b2e397a68ddd33467ccb97148f937580e4
AdamJi <jrgwyz@163.com>
2023-03-24 10:13:36 +0800

    test: update case

--- taos-ws-py/tests/test_req_id.py
+++ taos-ws-py/tests/test_req_id.py
@@ -7,7 +7,7 @@ config = [
         'db_protocol': 'taosws',
         'db_user': "root",
         'db_pass': "taosdata",
-        'db_host': "td-1",
+        'db_host': "localhost",
         'db_port': 6041,
         'db_name': "t_ws",
     }
@@ -32,27 +32,27 @@ def ctx(request):
     conn.close()
 
 
-def test_query(ctx):
+def test_query_simple(ctx):
     conn, db = ctx
     # res = conn.query_with_req_id('show dnodes', 1)
     res = conn.query('show dnodes')
     print(f'res: {res}')
 
 
-def test_execute(ctx):
+def test_execute_with_req_id_simple(ctx):
     conn, db = ctx
     res = conn.execute_with_req_id('show dnodes', 1)
     print(f'res: {res}')
 
 
-def test_cursor_execute(ctx):
+def test_cursor_execute_with_req_id_simple(ctx):
     conn, db = ctx
     cur = conn.cursor()
     res = cur.execute_with_req_id('show dnodes', 1)
     print(f'res: {res}')
 
 
-def test_cursor_execute_many(ctx):
+def test_cursor_execute_many_with_req_id(ctx):
     conn, db = ctx
     ws = conn
     cur = ws.cursor()
@@ -85,17 +85,64 @@ def test_cursor_execute_many(ctx):
     print(f'res: {res}')
 
 
-def test_full(ctx):
+def test_execute(ctx):
+    conn, db = ctx
+    ws = conn
+    cur = ws.cursor()
+    res = cur.execute('show dnodes', 1)
+    print(f'res: {res}')
+    cur.execute("drop database if exists {}", db)
+    cur.execute("create database {}", db)
+    cur.execute("use {name}", name=db)
+    cur.execute("create stable stb (ts timestamp, v1 int) tags(t1 int)")
+
+    data = [
+        {
+            "name": "tb1",
+            "t1": 1,
+        },
+        {
+            "name": "tb2",
+            "t1": 2,
+        },
+        {
+            "name": "tb3",
+            "t1": 3,
+        }
+    ]
+
+    res = cur.execute_many(
+        "create table {name} using stb tags({t1})",
+        data
+    )
+    print(f'res: {res}')
+
+    ts = datetime.datetime.now().astimezone()
+    data = [
+        ("tb1", ts, 1),
+        ("tb2", ts, 2),
+        ("tb3", ts, 3),
+    ]
+    cur.execute_many(
+        "insert into {} values('{}', {})",
+        data,
+    )
+    cur.execute('select * from stb')
+    row = cur.fetchone()
+    print(f'row: {row}')
+    assert row is not None
+
+
+def test_execute_with_req_id(ctx):
     conn, db = ctx
     ws = conn
     cur = ws.cursor()
     res = cur.execute_with_req_id('show dnodes', 1)
     print(f'res: {res}')
-    db = "t_ws"
-    cur.execute("drop database if exists {}", db)
-    cur.execute("create database {}", db)
-    cur.execute("use {name}", name=db)
-    cur.execute("create stable stb (ts timestamp, v1 int) tags(t1 int)")
+    cur.execute_with_req_id("drop database if exists {}", db)
+    cur.execute_with_req_id("create database {}", db)
+    cur.execute_with_req_id("use {name}", name=db)
+    cur.execute_with_req_id("create stable stb (ts timestamp, v1 int) tags(t1 int)")
 
     data = [
         {
