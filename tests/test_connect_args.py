5d74d44106698e7febc1985334234865e13b2ce1
AdamJi <jrgwyz@163.com>
2023-12-02 13:42:08 +0800

    tests: update case

--- tests/test_connect_args.py
+++ tests/test_connect_args.py
@@ -8,7 +8,7 @@ def test_connect_args():
     Useless args, prevent mistakenly deleted args in connect init.
     Because some case in CI of earlier version may use it.
     """
-    host = 'localhost:6030'
+    host = "localhost:6030"
     conn = connect(host)
     assert conn is not None
     conn.close()
