593d6791feeaba2560f7e1388cd53254100a8025
Alex Duan <417921451@qq.com>
2024-09-18 13:35:36 +0800

    fix: stmt2 query basic test passed

--- tests/test_stmt2.py
+++ tests/test_stmt2.py
@@ -16,7 +16,7 @@ def checkResultCorrects(conn, tbnames, tags, datas):
     pass
 
 # performace is high
-def test_bind_param(conn, stmt2):
+def insert_bind_param(conn, stmt2):
     # 
     #  table info , write 5 lines to 3 child tables d0, d1, d2 with super table
     #
@@ -65,7 +65,7 @@ def test_bind_param(conn, stmt2):
 
 
 # performance is lower
-def test_bind_param_with_tables(conn, stmt2):
+def insert_bind_param_with_tables(conn, stmt2):
 
     tbanmes = ["t1", "t2", "t3"]
     tags    = [
@@ -123,35 +123,120 @@ def test_bind_param_with_tables(conn, stmt2):
     checkResultCorrects(conn, tbanmes, tags, datas)
 
 
-def test_stmt2_insert(conn):
-    # type: (TaosConnection) -> None
-    dbname  = "stmt2"
-    stbname = "meters"
+#
+# insert
+#
+def test_stmt2_insert(conn, dbname, stbname):
     try:
-        conn.execute("drop database if exists %s" % dbname)
-        conn.execute("create database if not exists %s" % dbname)
-        conn.select_db(dbname)
-
-        sql = f"create table if not exists {stbname}(ts timestamp, name binary(32), sex bool, score int) tags(grade binary(24), class int)"
-        conn.execute(sql)
-
         # prepare
         stmt2 = conn.statement2(f"insert into ? using {stbname} tags(?,?) values(?,?,?,?)")
-        # conn.load_table_info("log")
 
         # insert with table
-        #test_bind_param_with_tables(conn, stmt2)
+        #insert_bind_param_with_tables(conn, stmt2)
 
         # insert with split args
-        test_bind_param(conn, stmt2)
+        insert_bind_param(conn, stmt2)
 
         #conn.execute("drop database if exists %s" % dbname)
+        stmt2.close()
         print("test_stmt2_insert test successful.")
 
     except Exception as err:
-        conn.execute("drop database if exists %s" % dbname)
+        #conn.execute("drop database if exists %s" % dbname)
         raise err
 
+
+#
+#  ------------------------ query -------------------
+#
+def query_bind_param(conn, stmt2):
+    # set param
+    #tbanmes = ["d2"]
+    tbanmes = None
+    tags    = None
+    datas   = [
+        # class 1
+        [
+            # where name in ('Tom2','alex2') or score > 1000;"
+            ["Tom2"],
+            [1000]
+        ]
+    ]
+
+    # set param
+    types = [FieldType.C_BINARY, FieldType.C_INT]
+    stmt2.set_columns_type(types)
+
+    # bind
+    stmt2.bind_param(tbanmes, tags, datas)
+
+
+# compare
+def compare_result(conn, sql2, res2):
+    
+
+    lres1 = []
+    lres2 = []
+   
+    # shor res2
+    for row in res2:
+        print(f" res2 rows = {row} \n")
+        lres2.append(row)
+
+    res1 = conn.query(sql2)
+    for row in res1:
+        print(f" res1 rows = {row} \n")
+        lres1.append(row)
+
+    row1 = len(lres1)
+    row2 = len(lres2)
+    col1 = len(lres1[0])
+    col2 = len(lres2[0])
+
+    # check number
+    if row1 != row2:
+        err = f"two results row count different. row1={row1} row2={row2}"
+        raise(BaseException(err))
+    if col1 != col2:
+        err = f" two results column count different. col1={col1} col2={col2}"
+        raise(BaseException(err))
+
+    for i in range(row1):
+        for j in range(col1):
+            if lres1[i][j] != lres2[i][j]:
+                raise(f" two results data different. i={i} j={j} data1={res1[i][j]} data2={res2[i][j]}\n")
+
+# query
+def test_stmt2_query(conn, dbname, stbname):
+    sql1 = "select * from d2 where name in (?) or score > ? ;"
+    sql2 = "select * from d2 where name in ('Tom2') or score > 1000;"
+
+    try:
+        # prepare
+        stmt2 = conn.statement2(sql1)
+        print("query prepare sql ............................. ok\n")
+
+        # bind
+        query_bind_param(conn, stmt2)
+        print("query bind param .............................. ok\n")
+
+        # query execute
+        stmt2.execute()
+        
+        # fetch result
+        res2 = stmt2.result()
+
+        # check result
+        compare_result(conn, sql2, res2)
+        print("query check corrent ........................... ok\n")
+
+        #conn.execute("drop database if exists %s" % dbname)
+        stmt2.close()
+        print("test_stmt2_query test successful.")
+
+    except Exception as err:
+        print("query ........................................ failed\n")
+        raise err
 
 
 if __name__ == "__main__":
@@ -162,7 +247,22 @@ if __name__ == "__main__":
 
     # test stmt2
     print("stmt2 bind and insert.\n")
-    test_stmt2_insert(conn)
+
+    dbname  = "stmt2"
+    stbname = "meters"
+    conn.execute("drop database if exists %s" % dbname)
+    conn.execute("create database if not exists %s" % dbname)
+    conn.select_db(dbname)
+    sql = f"create table if not exists {stbname}(ts timestamp, name binary(32), sex bool, score int) tags(grade binary(24), class int)"
+    conn.execute(sql)
+    
+
+    # insert 
+    test_stmt2_insert(conn, dbname, stbname)
+
+    # query
+    test_stmt2_query(conn, dbname, stbname)
+
 
     # close
     conn.close()
