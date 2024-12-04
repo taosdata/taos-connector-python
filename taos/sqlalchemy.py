583cd46387ceeeaf97c15ded39a6070208955e7b
Alex Duan <417921451@qq.com>
2024-12-04 15:46:09 +0800

    feat: support taos taosws taosrest protocol for superset

--- taos/sqlalchemy.py
+++ taos/sqlalchemy.py
@@ -1,33 +1,39 @@
+import sys
 from sqlalchemy import types as sqltypes
 from sqlalchemy.engine import default, reflection
 from sqlalchemy import text
 
 TYPES_MAP = {
-    "bool": sqltypes.Boolean,
-    "timestamp": sqltypes.DATETIME,
-    "tinyint": sqltypes.SmallInteger,
-    "smallint": sqltypes.SmallInteger,
-    "int": sqltypes.Integer,
-    "bigint": sqltypes.BigInteger,
-    "tinyint unsigned": sqltypes.SmallInteger,
-    "smallint unsigned": sqltypes.SmallInteger,
-    "int unsigned": sqltypes.Integer,
-    "bigint unsigned": sqltypes.BigInteger,
-    "float": sqltypes.FLOAT,
-    "double": sqltypes.DECIMAL,
-    "nchar": sqltypes.String,
-    "binary": sqltypes.String,
-    "varchar": sqltypes.String,
-    "varbinary": sqltypes.String,
+    "BOOL"             : sqltypes.Boolean,
+    "TIMESTAMP"        : sqltypes.DATETIME,
+    "INT"              : sqltypes.Integer,
+    "INT UNSIGNED"     : sqltypes.Integer,
+    "BIGINT"           : sqltypes.BigInteger,
+    "BIGINT UNSIGNED"  : sqltypes.BigInteger,
+    "FLOAT"            : sqltypes.FLOAT,
+    "DOUBLE"           : sqltypes.DECIMAL,
+    "TINYINT"          : sqltypes.SmallInteger,
+    "TINYINT UNSIGNED" : sqltypes.SmallInteger,
+    "SMALLINT"         : sqltypes.SmallInteger,
+    "SMALLINT UNSIGNED": sqltypes.SmallInteger,
+    "BINARY"           : sqltypes.String,
+    "VARCHAR"          : sqltypes.String,
+    "VARBINARY"        : sqltypes.String,
+    "NCHAR"            : sqltypes.Unicode,
+    "JSON"             : sqltypes.JSON,
 }
 
 
-class TaosWsDialect(default.DefaultDialect):
-    name = "taosws"
-    driver = "taosws"
+#
+#  base class for dialect
+#
+class BaseDialect(default.DefaultDialect):
     supports_native_boolean = True
     implicit_returning = True
     supports_statement_cache = True
+
+    def is_sys_db(self, dbname):
+        return dbname.lower() in [ "information_schema", "performance_schema"]
 
     def do_rollback(self, connection):
         pass
@@ -36,73 +42,128 @@ class TaosWsDialect(default.DefaultDialect):
         cursor = connection.execute(text("select server_version()"))
         return cursor.fetchone()
 
-    @classmethod
-    def dbapi(cls):
-        import taosws
-
-        return taosws
-
-    @classmethod
-    def import_dbapi(cls):
-        import taosws
-        return taosws
-
-    def has_schema(self, connection, schema):
-        return False
-
-    def has_table(self, connection, table_name, schema=None):
-        try:
-            connection.execute(text("describe {}" % table_name))
-            return True
-        except:
-            return False
-
     @reflection.cache
-    def get_indexes(self, connection, table_name, schema=None, **kw):
-        """
-        Gets all indexes
-        """
-        # no index is supported by TDengine
-        return []
+    def has_schema(self, connection, schema):
+        return schema in self.get_schema_names(connection)
 
+    # has table
+    @reflection.cache
+    def has_table(self, connection, table_name, schema=None):
+        return table_name in self.get_table_names(connection, schema)
+
+    # get column
+    @reflection.cache
     def get_columns(self, connection, table_name, schema=None, **kw):
+        sysdb = False
+        if schema is None:
+            sql = f"describe {table_name}"
+        else:
+            sql = f"describe {schema}.{table_name}"
+            sysdb = self.is_sys_db(schema)
         try:
-            cursor = connection.execute(text("describe {}" % table_name))
-            return [row[0] for row in cursor.fetchall()]
+            cursor = connection.execute(sql)
+            columns = []
+            for row in cursor.fetchall():
+                #print(row)
+                column = dict()
+                column["name"] = row[0]
+                column["type"] = self._resolve_type(row[1])
+                columns.append(column)
+            return columns
         except:
             return []
 
+    @reflection.cache
+    def get_pk_constraint(self, connection, table_name, schema=None, **kw):        
+        columns = self.get_columns(connection, table_name, schema)
+        return {"constrained_columns": [columns[0]["name"]], "name": None}
+
+    @reflection.cache
+    def get_foreign_keys(self, connection, table_name, schema=None, **kw):
+        # no foreign key is supported by TDengine
+        return []
+
+    # get indexs
+    @reflection.cache
+    def get_indexes(self, connection, table_name, schema=None, **kw):
+        sql = (
+            "SELECT * FROM information_schema.INS_INDEXES "
+            f"WHERE db_name = '{schema}'"
+            f"AND table_name = '{table_name}'"
+        )
+        try:
+            cursor = connection.execute(sql)
+            rows = cursor.fetchall()
+            indexes = []
+            for row in rows:
+                index = {"name": row[0], "column_names": [row[5]], "type": "index", "unique": False}
+                indexes.append(index)
+            return indexes
+        except:
+            return []
+
+    # get database name
+    @reflection.cache
+    def get_schema_names(self, connection, **kw):
+        sql = "select name from information_schema.ins_databases where `vgroups` is not null"
+        try:
+            cursor = connection.execute(sql)
+            return [row[0] for row in cursor.fetchall()]
+        except:
+            return []
+    
+    # get table names
+    @reflection.cache
+    def get_table_names(self, connection, schema=None, **kw):
+        if schema is None:
+            return []
+        # sql
+        sqls = [
+            f"select stable_name from information_schema.ins_stables where db_name = '{schema}'",
+            f"select  table_name from information_schema.ins_tables  where db_name = '{schema}' and type='NORMAL_TABLE'"]
+        # execute
+        try:
+            names = []
+            for sql in sqls:
+                cursor = connection.execute(sql)
+                for row in cursor.fetchall():
+                    names.append(row[0])
+            return names
+        except:
+            return []
+
+    @reflection.cache
+    def get_view_names(self, connection, schema=None, **kw):
+        return []
+
     def _resolve_type(self, type_):
+        #print(f"call function {sys._getframe().f_code.co_name} type: {type_} ...\n")
         return TYPES_MAP.get(type_, sqltypes.UserDefinedType)
 
 
+#
+# ---------------- taos impl -------------
+#
+import taos
+
+#
+# Alchemy connect
+#
 class AlchemyTaosConnection:
     paramstyle = "pyformat"
-
+    # connect
     def connect(self, **kwargs):
         host = kwargs.get("host", "localhost")
         port = kwargs.get("port", "6030")
         user = kwargs.get("username", "root")
         password = kwargs.get("password", "taosdata")
         database = kwargs.get("database", None)
-
-        import taos
-
         return taos.connect(host=host, user=user, password=password, port=int(port), database=database)
 
-
-class TaosDialect(default.DefaultDialect):
+# taos dialet
+class TaosDialect(BaseDialect):
     name = "taos"
     driver = "taos"
-    supports_native_boolean = True
-    implicit_returning = True
-    supports_statement_cache = True
-
-    def do_rollback(self, connection):
-        pass
-
-    def _get_server_version_info(self, connection):
-        return tuple(connection.connection.server_info)
 
     @classmethod
     def dbapi(cls):
@@ -112,30 +173,24 @@ class TaosDialect(default.DefaultDialect):
     def import_dbapi(cls):
         return AlchemyTaosConnection()
 
-    def has_schema(self, connection, schema):
-        return False
 
-    def has_table(self, connection, table_name, schema=None):
-        try:
-            connection.execute(text("describe {}" % table_name))
-            return True
-        except:
-            return False
+#
+# ---------------- taosws impl -------------
+#
+import taosws
 
-    @reflection.cache
-    def get_indexes(self, connection, table_name, schema=None, **kw):
-        """
-        Gets all indexes
-        """
-        # no index is supported by TDengine
-        return []
+# ws dailet
+class TaosWsDialect(BaseDialect):
+    # set taosws
+    name = "taosws"
+    driver = "taosws"
 
-    def get_columns(self, connection, table_name, schema=None, **kw):
-        try:
-            cursor = connection.execute(text("describe {}" % table_name))
-            return [row[0] for row in cursor.fetchall()]
-        except:
-            return []
+    # doapi
+    @classmethod
+    def dbapi(cls):
+        return taosws
 
-    def _resolve_type(self, type_):
-        return TYPES_MAP.get(type_, sqltypes.UserDefinedType)
+    # import dbapi
+    @classmethod
+    def import_dbapi(cls):
+        return taosws
--- tests/test_sqlalchemy.py
+++ tests/test_sqlalchemy.py
@@ -1,12 +1,11 @@
 import taos
-
+import taosws
+import taosrest
+import datetime
+from sqlalchemy import types as sqltypes
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
-
 from dotenv import load_dotenv
-
-from utils import tear_down_database
-
 load_dotenv()
 
 
@@ -15,86 +14,108 @@ def test_insert_test_data():
     c = conn.cursor()
     c.execute("drop database if exists test")
     c.execute("create database test")
-    c.execute("create table test.tb " "(ts timestamp, c1 int, c2 double)")
-    c.execute("insert into test.tb values " "(now, -100, -200.3) " "(now+10s, -101, -340.2423424)")
+    c.execute("create table test.meters (ts timestamp, c1 int, c2 double) tags(t1 int)")
+    c.execute("insert into test.d0 using test.meters tags(0) values (1733189403001, 1, 1.11) (1733189403002, 2, 2.22)")
+    c.execute("insert into test.d1 using test.meters tags(1) values (1733189403003, 3, 3.33) (1733189403004, 4, 4.44)")
+    c.execute("create table test.ntb(ts timestamp, age int)")
+    c.execute("insert into test.ntb values(now, 23)")
 
+# compare list
+def checkListEqual(list1, list2, tips):
+    if list1 != list2:
+        print(f"{tips} failed. two list item not equal. list1={list1} list2={list2}")
+        raise("list not euqal.")
 
+# check result
+def checkResultEqual(result1, result2, tips):
+    if result1 != result2:
+        print(f"{tips} failed. result not equal. result1={result1} result2={result2}")
+        raise("result not euqal.")
+
+# check baisc function
+def checkBasic(conn, inspection):
+    # get schema names
+    checkListEqual(inspection.get_schema_names(), ["test"], "check get_schema_names()")
+    # get table names
+    tables = ['meters','ntb']
+    checkListEqual(inspection.get_table_names("test"), tables, "check get_table_names()")
+
+    # get_columns
+    cols2 = [
+        {'name': 'ts', 'type': inspection.dialect._resolve_type("TIMESTAMP")}, 
+        {'name': 'c1', 'type': inspection.dialect._resolve_type("INT")}, 
+        {'name': 'c2', 'type': inspection.dialect._resolve_type("DOUBLE")}, 
+        {'name': 't1', 'type': inspection.dialect._resolve_type("INT")}
+    ]
+    cols1 = inspection.get_columns("meters", "test")
+    for i in range(len(cols1)):
+        cname1 = cols1[i]['name']
+        ctype1 = cols1[i]['type']
+        cname2 = cols2[i]['name']
+        ctype2 = cols2[i]['type']
+
+        if cname1 != cname2:
+            print(f"two name diff, name1={cname1} name2={cname2}")
+            raise("name diff")
+
+        if type(ctype1) != ctype2:
+            print(f"two type diff, type1={ctype1} | {type(ctype1)} type2={str(ctype2)} | {type(ctype2)}")
+            raise("type diff")
+
+    # has tabled
+    checkResultEqual(inspection.has_table("meters", "test"), True, "check has_table()")
+    # has_schema
+    checkResultEqual(inspection.dialect.has_schema(conn, "test"), True, "check has_schema()")
+
+    # get_indexes
+    print("inspection.get_indexes", inspection.get_indexes("test.meters"))
+
+    res = conn.execute("select * from test.meters")
+    print("res", res.fetchall())
+
+    # import_dbapi
+    print("inspection.dialect.import_dbapi", inspection.dialect.import_dbapi())
+
+    # _resolve_type
+    print("inspection.dialect._resolve_type", inspection.dialect._resolve_type("int"))
+
+    conn.close()
+
+# taos
 def test_read_from_sqlalchemy_taos():
     if not taos.IS_V3:
         return
     engine = create_engine("taos://root:taosdata@localhost:6030?timezone=Asia/Shanghai")
     conn = engine.connect()
     inspection = inspect(engine)
+    checkBasic(conn, inspection)
 
-    print("inspection.get_schema_names()", inspection.get_schema_names())
-
-    print("inspection.has_table", inspection.has_table("test.tb"))
-
-    # has_schema
-    print("inspection.has_schema", inspection.dialect.has_schema(conn, "test"))
-
-    # get_columns
-    print("inspection.get_columns", inspection.get_columns("test.tb"))
-
-    # get_indexes
-    print("inspection.get_indexes", inspection.get_indexes("test.tb"))
-
-    res = conn.execute("select * from test.tb")
-    print("res", res.fetchall())
-
-    # import_dbapi
-    print("inspection.dialect.import_dbapi", inspection.dialect.import_dbapi())
-
-    # _resolve_type
-    print("inspection.dialect._resolve_type", inspection.dialect._resolve_type("int"))
-
-    conn.close()
-
-
+# taosws
 def test_read_from_sqlalchemy_taosws():
-    try:
-        import taosws
-    except ImportError:
-        return
     if not taos.IS_V3:
         return
     engine = create_engine("taosws://root:taosdata@localhost:6041?timezone=Asia/Shanghai")
     conn = engine.connect()
     inspection = inspect(engine)
+    checkBasic(conn, inspection)
 
-    print("inspection.get_schema_names()", inspection.get_schema_names())
+# taosrest
+def test_read_from_sqlalchemy_taosrest():
+    if not taos.IS_V3:
+        return
+    engine = create_engine("taosrest://root:taosdata@localhost:6041?timezone=Asia/Shanghai")
+    conn = engine.connect()
+    inspection = inspect(engine)
+    checkBasic(conn, inspection)
 
-    print("inspection.has_table", inspection.has_table("test.tb"))
-
-    # has_schema
-    print("inspection.has_schema", inspection.dialect.has_schema(conn, "test"))
-
-    # get_columns
-    print("inspection.get_columns", inspection.get_columns("test.tb"))
-
-    # get_indexes
-    print("inspection.get_indexes", inspection.get_indexes("test.tb"))
-
-    res = conn.execute("select * from test.tb")
-    print("res", res.fetchall())
-
-    # import_dbapi
-    print("inspection.dialect.import_dbapi", inspection.dialect.import_dbapi())
-
-    # _resolve_type
-    print("inspection.dialect._resolve_type", inspection.dialect._resolve_type("int"))
-
-    conn.close()
-
-
-def teardown_module(module):
-    conn = taos.connect()
-    db_name = "test"
-    tear_down_database(conn, db_name)
-    conn.close()
-
-
+# main test
 if __name__ == "__main__":
+    print("hello, test sqlalcemy db api.\n")
     test_insert_test_data()
+    print("Insert table and data ............................. [OK]\n")
     test_read_from_sqlalchemy_taos()
+    print("Test taos api ..................................... [OK]\n")
     test_read_from_sqlalchemy_taosws()
+    print("Test taosws api ................................... [OK]\n")
+    test_read_from_sqlalchemy_taosrest()
+    print("Test taosrest api ................................. [OK]\n")
