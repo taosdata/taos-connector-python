a0ed01970c3af74780c0c85bb672cb02426bfe4b
Alex Duan <417921451@qq.com>
2024-12-12 17:14:50 +0800

    TDengine.py add time grain

--- superset/db_engine_specs/TDengine.py
+++ superset/db_engine_specs/TDengine.py
@@ -2,9 +2,18 @@
 from superset.db_engine_specs.base import  BaseEngineSpec
 
 class TDengineEngineSpec(BaseEngineSpec):
-    engine_name = "TDengine"  # show name in SuperSet driver list
-    engine      = "taosws"
+    engine = "taosws"
+    engine_name = "TDengine"
     max_column_name_length = 64
     default_driver = "taosws"
-    sqlalchemy_uri_placeholder = (
-        "protocol://user:password@host:port/dbname[?key=value&key=value...]"    )
+    sqlalchemy_uri_placeholder = ("taosws://user:password@host:port/dbname[?key=value&key=value...]")
+    
+    # time grain
+    _time_grain_expressions = {
+        None: "{col}",
+        "PT1S": "TIMETRUNCATE({col}, 1s, 0)",
+        "PT1M": "TIMETRUNCATE({col}, 1m, 0)",
+        "PT1H": "TIMETRUNCATE({col}, 1h, 0)",
+        "P1D":  "TIMETRUNCATE({col}, 1d, 0)",
+        "P1W":  "TIMETRUNCATE({col}, 1w, 0)",
+    }
