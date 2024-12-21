48387144cfee899fa3e33b7740ee322ab44fe4cb
Alex Duan <417921451@qq.com>
2024-12-21 17:03:34 +0800

    fix: add CLOUD_DSN replace plain text

--- taos-ws-py/tests/test_cloud.py
+++ taos-ws-py/tests/test_cloud.py
@@ -1,11 +1,19 @@
+import os
 import taosws
 
 def test_ws_connect():
     print("-" * 40)
     print("test_ws_connect")
-    conn = taosws.connect("%s?token=%s" % ('wss://gw.cloud.taosdata.com', '68d41c020d2e9e3dcac18140460d1bcfd82d642d'))
-    r = conn.query_with_req_id("show dnodes", 1)
-    print("r: ", r.fields)
+    # get evn
+    token = os.environ.get("CLOUD_TOKEN")
+    print(token)
+    conn = taosws.connect("%s?token=%s" % ('wss://gw.cloud.taosdata.com', token))
+    res = conn.query_with_req_id("show databases", 1)
+    print(res)
+    dbs = [row[0] for row in res]
+    # check sys db exist
+    assert "information_schema" in dbs
+    assert "performance_schema" in dbs
     print("test_ws_connect done")
     print("-" * 40)
 
