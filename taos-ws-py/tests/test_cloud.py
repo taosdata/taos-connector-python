fcfa1b78c125b3f86ccded81213ce2d7317c55a2
Alex Duan <417921451@qq.com>
2024-12-22 10:50:48 +0800

    test: token pass with argument

--- taos-ws-py/tests/test_cloud.py
+++ taos-ws-py/tests/test_cloud.py
@@ -1,16 +1,17 @@
 import os
+import sys
 import taosws
 
-def test_ws_connect():
+def test_ws_connect(token):
     print("-" * 40)
     print("test_ws_connect")
     # get evn
-    token = os.environ.get("CLOUD_TOKEN")
+    #token = os.environ.get("CLOUD_TOKEN")
     print(token)
     conn = taosws.connect("%s?token=%s" % ('wss://gw.cloud.taosdata.com', token))
     res = conn.query_with_req_id("show databases", 1)
-    print(res)
     dbs = [row[0] for row in res]
+    print(dbs)
     # check sys db exist
     assert "information_schema" in dbs
     assert "performance_schema" in dbs
@@ -18,4 +19,7 @@ def test_ws_connect():
     print("-" * 40)
 
 if __name__ == "__main__":
-    test_ws_connect()
+    if len(sys.argv) == 0:
+        print("need token argument passed.\n")
+    else:
+        test_ws_connect(sys.argv[0])
