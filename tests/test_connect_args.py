84ecc18b882265f7716289a018e8531c75d61cc6
menshibin <men_shi_bin@163.com>
2025-05-12 10:17:08 +0800

    feat: add connect option functions

--- tests/test_connect_args.py
+++ tests/test_connect_args.py
@@ -8,7 +8,21 @@ def test_connect_args():
     Useless args, prevent mistakenly deleted args in connect init.
     Because some case in CI of earlier version may use it.
     """
-    host = "localhost:6030"
+    host = "192.168.2.140"
     conn = connect(host)
+    conn.options_connection(0, "utf8")
+    conn.options_connection(1, "UTC")
+    conn.options_connection(2, "127.0.0.2")
+    conn.options_connection(3, "python client")
+    conn.set_conn_mode(0, 1)
+    
+    result = conn.query("select last(*) from test.meters")
+    data = result.fetch_all()
+    print(data)
+    assert data is None
+    # assert rows is not None
+    # print(rows)
+    # for row in result:
+    #     print(row)
     assert conn is not None
     conn.close()
