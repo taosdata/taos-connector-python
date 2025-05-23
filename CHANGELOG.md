cf0e26d161646f9d1deb2dae0818e29ebb29417a
kevin men <men_shi_bin@163.com>
2025-05-23 15:39:02 +0800

    feat: add connect property setting function to native connector (#328)
    * feat: add connect property setting function to native connector
    
    * feat: add options connection case
    
    * feat: add options connection case
    
    * feat: modify options connection case for ci test
    
    * feat: adjust options connection case param for ci test
    
    * feat: modify options connection case check
    
    * fix: delete libtaos.so version
    
    * feat: modify mac yml for ci test
    
    * test: modify mac.yml print lib dir
    
    * feat: delete libtaos ln
    
    * feat: add ci test command
    
    * feat: add ci test env info print
    
    * feat: modify mac.yml for print test
    
    * feat: modify libtaos path for test
    
    * feat: Delete incorrect soft connections
    
    * feat: modify adapter soft connections
    
    * feat: clear libtaos
    
    * feat: modify spell error
    
    * feat: add local lib dir print
    
    * feat: modify soft link shell
    
    * feat: modify taosc soft link for taosadapter
    
    * feat: delete test code
    
    * feat: restore connector parameters
    
    * feat: put the options/mode settings to connect() parameters
    
    * feat: rename TSDB_OPTION_CONNECTION

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,30 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## v2.8.1 - 2025-05-22
+
+### Features:
+
+- add connect property setting function to native connector
+
+### Bug Fixes:
+
+- all workflows cmake config
+- delete token auth
+- modify version to 2.8.0
+- optimize null value checks (#317)
+- password supports special characters
+- remove superset driver TDengine.py
+- test auth base64
+- uniform python name
+- workflows cmake config
+- **(ws)**: update Cargo.lock
+
+### Documents:
+
+- add changelog for v2.7.22/v2.7.23
+- python to Python
+
 ## v2.7.23 - 2025-03-24
 - support DECIMAL data type
 
--- tests/test_connect_args.py
+++ tests/test_connect_args.py
@@ -1,5 +1,7 @@
 from taos import *
+import time
 
+from taos.constants import TSDB_CONNECTIONS_MODE, TSDB_OPTION_CONNECTION
 
 def test_connect_args():
     """
@@ -8,7 +10,55 @@ def test_connect_args():
     Useless args, prevent mistakenly deleted args in connect init.
     Because some case in CI of earlier version may use it.
     """
+ 
     host = "localhost:6030"
-    conn = connect(host)
+    conn = connect(host=host, charset="utf8", timezone="UTC", user_app="python client 1", user_ip="127.2.2.2", bi_mode=True)
+   
+    time.sleep(30)
+    bClient = False
+    bHost = False
+    result = conn.query("show connections")
+    assert result is not None
+    for row in result:
+        print(row)
+        if row[7] == "python client 1":
+            bClient = True
+        if row[8] == "127.2.2.2":
+            bHost = True
+
+    assert bClient and bHost
+    
+    result = conn.query("select timezone()")
+    assert result is not None
+    for row in result:
+        print(row)
+        assert row[0] == "UTC (UTC, +0000)"
+
+    conn.set_option(TSDB_OPTION_CONNECTION.TSDB_OPTION_CONNECTION_CHARSET.value, "utf8")
+    conn.set_option(TSDB_OPTION_CONNECTION.TSDB_OPTION_CONNECTION_TIMEZONE.value, "UTC")
+    conn.set_option(TSDB_OPTION_CONNECTION.TSDB_OPTION_CONNECTION_USER_IP.value, "127.0.0.2")
+    conn.set_option(TSDB_OPTION_CONNECTION.TSDB_OPTION_CONNECTION_USER_APP.value, "python client")
+    conn.set_mode(TSDB_CONNECTIONS_MODE.TSDB_CONNECTIONS_MODE_BI.value, 1)
+    
+    time.sleep(30)
+    bClient = False
+    bHost = False
+    result = conn.query("show connections")
+    assert result is not None
+    for row in result:
+        print(row)
+        if row[7] == "python client":
+            bClient = True
+        if row[8] == "127.0.0.2":
+            bHost = True
+
+    assert bClient and bHost
+    
+    result = conn.query("select timezone()")
+    assert result is not None
+    for row in result:
+        print(row)
+        assert row[0] == "UTC (UTC, +0000)"
+    
     assert conn is not None
     conn.close()
