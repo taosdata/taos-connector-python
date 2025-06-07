4a248a9e4b851b5f9322d5ddc13ae36f39868907
menshibin <men_shi_bin@163.com>
2025-06-07 16:49:00 +0800

    fix: modify cloud insert sql

--- taos-ws-py/tests/test_cloud_tmq.py
+++ taos-ws-py/tests/test_cloud_tmq.py
@@ -21,10 +21,10 @@ def prepareMeta():
         conn = taosws.connect("wss://%s?token=%s" % (url, token))
         sql = """
             INSERT INTO 
-            power.d1001 USING dmeters.meters (groupid, location) TAGS(2, 'California.SanFrancisco')
+            dmeters.d1001 USING dmeters.meters (groupid, location) TAGS(2, 'SanFrancisco')
                 VALUES (NOW + 1a, 10.30000, 219, 0.31000) 
                 (NOW + 2a, 12.60000, 218, 0.33000) (NOW + 3a, 12.30000, 221, 0.31000)
-            power.d1002 USING dmeters.meters (groupid, location) TAGS(3, 'California.SanFrancisco') 
+            dmeters.d1002 USING dmeters.meters (groupid, location) TAGS(3, 'SanFrancisco') 
                 VALUES (NOW + 1a, 10.30000, 218, 0.25000)
             """
         affectedRows = conn.execute(sql)
