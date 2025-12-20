41711784cfa67540136889715b73e64309f9575d
qevolg <2227465945@qq.com>
2025-12-20 22:01:39 +0800

    refactor: improve git_commit_id retrieval logic in get_connector_info function

--- taos-ws-py/tests/utils.py
+++ taos-ws-py/tests/utils.py
@@ -7,13 +7,17 @@ def get_connector_info():
     cargo_toml = toml.load(os.path.join(os.path.dirname(__file__), "../Cargo.toml"))
     version = cargo_toml["package"]["version"]
 
-    git_commit_id = (
-        subprocess.check_output(
-            ["git", "rev-parse", "--short=7", "HEAD"],
-            cwd=os.path.dirname(os.path.abspath(__file__)),
+    git_commit_id = os.getenv("GIT_COMMIT_ID")
+    if not git_commit_id:
+        git_commit_id = (
+            subprocess.check_output(
+                ["git", "rev-parse", "--short=7", "HEAD"],
+                cwd=os.path.dirname(os.path.abspath(__file__)),
+            )
+            .decode()
+            .strip()
         )
-        .decode()
-        .strip()
-    )
+    if not git_commit_id:
+        git_commit_id = "ncid000"
 
     return f"python-ws-v{version}-{git_commit_id}"
