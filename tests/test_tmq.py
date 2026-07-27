cff99ceaf7a588102c180b75473547e54661ea47
qevolg <ethanguo@taosdata.com>
2026-07-27 16:27:03 +0800

    test: fix failed test case

--- tests/test_tmq.py
+++ tests/test_tmq.py
@@ -526,7 +526,10 @@ def test_tmq_with_token():
 def test_tmq_with_invalid_token():
     pre_test_tmq("")
     try:
-        with pytest.raises(TmqError, match=r"Invalid token"):
+        with pytest.raises(
+            TmqError,
+            match=r"(Invalid token|init tscObj with token failed)",
+        ):
             Consumer(
                 {
                     "group.id": "token_test_group",
